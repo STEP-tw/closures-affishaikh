f23c4e26cf968f35a6ff96d88401f34b37bc78b6
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-10-25 11:21:48 +0530

    Defined the function makeCounterFromZero. All tests have passed.

--- src/lib.js
+++ src/lib.js
@@ -10,7 +10,14 @@ const makeCounterFromN = function(startingCount) {
     return startingCount++;
   }
 };
-const makeCounterFromZero = undefined;
+
+const makeCounterFromZero = function() {
+  let startingCount = 0;
+  return function() {
+    return startingCount++;
+  }
+};
+
 const makeDeltaTracker = undefined;
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
